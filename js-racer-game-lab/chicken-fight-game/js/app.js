f60ad210121121f05f029579fb1d316664aea2df
addison912 <addisondrewmoore@gmail.com>
2018-11-09 15:16:26 -0800

    fixed bugs and added reset button

--- js-racer-game-lab/chicken-fight-game/js/app.js
+++ js-racer-game-lab/chicken-fight-game/js/app.js
@@ -1,14 +1,17 @@
+document.getElementById("start").addEventListener("click", start);
+
 class player {
-  constructor(name, position, health, keys, div) {
+  constructor(name, position, health, keys, div, direction) {
     this.name = name;
     this.position = position;
     this.health = health;
     //keys takes an array [up, right, down, left, punch]
     this.keys = keys;
     this.div = div;
+    this.direction = direction;
+    this.div.style.display = "inline-block";
   }
   move(key) {
-    console.log;
     switch (key) {
       case this.keys[0]:
         if (this.position[1] > 200) {
@@ -20,7 +23,7 @@ class player {
           this.position[0] += 10;
         }
         this.div.setAttribute("src", `../images/${this.name}-right.png`);
-        direction = "right";
+        this.direction = "right";
         break;
       case this.keys[2]:
         this.position[1] += 10;
@@ -28,50 +31,67 @@ class player {
       case this.keys[3]:
         this.position[0] -= 10;
         this.div.setAttribute("src", `../images/${this.name}-left.png`);
-        direction = "left";
+        this.direction = "left";
         break;
       case this.keys[4]:
-        console.log(direction);
-        direction == "left"
-          ? this.div.setAttribute(
-              "src",
-              `../images/${this.name}-left-punch.png`
-            )
-          : this.div.setAttribute(
-              "src",
-              `../images/${this.name}-right-punch.png`
-            );
+        if (this.direction == "left") {
+          this.punchLeft();
+        } else {
+          this.punchRight();
+        }
         break;
     }
     this.div.style.left = `${this.position[0]}px`;
     this.div.style.top = `${this.position[1]}px`;
     this.div.style.zIndex = this.position[1];
   }
+  reset(key) {
+    switch (key) {
+      case this.keys[4]:
+        if (this.direction == "left") {
+          this.div.setAttribute("src", `../images/${this.name}-left.png`);
+        } else {
+          this.div.setAttribute("src", `../images/${this.name}-right.png`);
+        }
+    }
+  }
+  punchRight() {
+    this.div.setAttribute("src", `../images/${this.name}-right-punch.png`);
+  }
+  punchLeft() {
+    this.div.setAttribute("src", `../images/${this.name}-left-punch.png`);
+  }
 }
 
-peter = new player(
-  "peter",
-  [900, 350],
-  5,
-  [38, 39, 40, 37, 93],
-  document.getElementById("peter")
-);
-
-chicken = new player(
-  "chicken",
-  [200, 350],
-  5,
-  [87, 68, 83, 65, 32],
-  document.getElementById("chicken")
-);
-
-peter.move();
-chicken.move();
-
 let key;
-let direction;
 window.onkeydown = function(e) {
   key = e.keyCode;
   peter.move(key);
   chicken.move(key);
 };
+window.onkeyup = function(e) {
+  key = e.keyCode;
+  chicken.reset(key);
+  peter.reset(key);
+};
+
+function start() {
+  chicken = new player(
+    "chicken",
+    [200, 350],
+    5,
+    [87, 68, 83, 65, 67],
+    document.getElementById("chicken")
+  );
+  peter = new player(
+    "peter",
+    [900, 350],
+    5,
+    [38, 39, 40, 37, 18],
+    document.getElementById("peter")
+  );
+  peter.move();
+  chicken.move();
+}
+
+// start();
